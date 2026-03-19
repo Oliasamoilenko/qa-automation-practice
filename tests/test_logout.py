7a88c7001510c9930ef0e92afd83553e757d02a0
Olia Samoilenko <samojlenkoola7@gmail.com>
2026-03-19 18:29:37 +0000

    Fix logout test with proper wait

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 def test_logout(driver):
     driver.get("https://www.saucedemo.com/")
@@ -9,8 +11,14 @@ def test_logout(driver):
     driver.find_element(By.ID, "login-button").click()
 
     driver.find_element(By.ID, "react-burger-menu-btn").click()
-    time.sleep(1)
 
-    driver.find_element(By.ID, "logout_sidebar_link").click()
+    wait = WebDriverWait(driver, 10)
+
+    logout_button = wait.until(
+        EC.element_to_be_clickable((By.ID, "logout_sidebar_link"))
+    )
+    logout_button.click()
+
+    wait.until(EC.url_to_be("https://www.saucedemo.com/"))
 
     assert driver.current_url == "https://www.saucedemo.com/"
